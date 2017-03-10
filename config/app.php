8fd6e9b949a1ee38c7aba6626d81bfd0ebe23109
billtsai1518 <billtsai1518@gmail.com>
2017-03-10 10:44:11 +0800

    https://github.com/fideloper/TrustedProxy/blob/master/readme.md

--- config/app.php
+++ config/app.php
@@ -176,6 +176,10 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+		
+		//Laravel Trusted Proxies
+		//https://github.com/fideloper/TrustedProxy
+		Fideloper\Proxy\TrustedProxyServiceProvider::class,
 
     ],
 
