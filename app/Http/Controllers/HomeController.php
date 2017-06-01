99ef7af26cf99b0f11b9f36676329991475bcef8
billtsai1518 <billtsai1518@gmail.com>
2017-06-01 22:26:59 +0800

    add classes table, update users table, show class info in dashboard

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Classes;
+use App\User;
 use Illuminate\Http\Request;
 
 class HomeController extends Controller
@@ -23,6 +25,6 @@ class HomeController extends Controller
      */
     public function index()
     {
-        return view('dashboard');
+        return view('dashboard', ['classes' => Classes::all(), 'users' => User::all()]);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -6,14 +6,22 @@
 	<div class="page-header">
 		<h1>Dashboard</h1>
 	</div>
-	<div class="panel panel-info">
-		<div class="panel-heading">
-			<h3 class="panel-title">您的班級</h3>
-		</div>
-		<div class="panel-body">
-			<p>某班</p>
-		</div>
-	</div>
+	@foreach ($classes as $class)
+		@if (Auth::user()->id == $class->teacher_id)
+			<div class="panel panel-info">
+				<div class="panel-heading">
+					<h3 class="panel-title">{{ $class->name }}</h3>
+				</div>
+				<div class="panel-body">
+					@foreach ($users as $user)
+						@if ($class->id == $user->class_id && $class->teacher_id != $user->id)
+							<p>{{ $user->name }}</p>
+						@endif
+					@endforeach
+				</div>
+			</div>
+		@endif
+	@endforeach
 
 @endsection
 
