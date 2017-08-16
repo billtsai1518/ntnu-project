a0c58380fb3965dd7bd89357e9ab689e585f45e8
billtsai1518 <billtsai1518@gmail.com>
2017-08-16 12:29:50 +0800

    Set mail server

--- config/mail.php
+++ config/mail.php
@@ -56,8 +56,8 @@ return [
     */
 
     'from' => [
-        'address' => env('MAIL_FROM_ADDRESS', 'hello@example.com'),
-        'name' => env('MAIL_FROM_NAME', 'Example'),
+        'address' => env('MAIL_FROM_ADDRESS', 'postmaster@sandboxedff5826eb2948888729afdd7cdb4062.mailgun.org'),
+        'name' => env('MAIL_FROM_NAME', 'Puzzort'),
     ],
 
     /*
