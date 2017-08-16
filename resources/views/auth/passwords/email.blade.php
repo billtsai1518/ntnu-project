d86350fad1a05a95cd2ef81e4a45e2e127d6e282
billtsai1518 <billtsai1518@gmail.com>
2017-08-16 14:32:11 +0800

    update pages of reset password

--- resources/views/auth/passwords/email.blade.php
+++ resources/views/auth/passwords/email.blade.php
@@ -1,14 +1,18 @@
 @extends('layouts.app')
 
+@section('title', '忘記密碼 | ')
+
 @section('content')
+<div class="page-header">
+    <h1>忘記密碼</h1>
+</div>
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
-                <div class="panel-heading">Reset Password</div>
-                <div class="panel-body">
+            <div class="panel" style="border-color: #FF9898;">
+                <div class="panel-body" style="padding: 30px; border-color: #FF9898;">
                     @if (session('status'))
-                        <div class="alert alert-success">
+                        <div class="alert" style="color: black; background-color: #FFDDDD; border-color: #FFDDDD;">
                             {{ session('status') }}
                         </div>
                     @endif
@@ -32,13 +36,14 @@
 
                         <div class="form-group">
                             <div class="col-md-6 col-md-offset-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn" style="color: white; background-color: #FF9898; border-color: #FF9898;">
                                     Send Password Reset Link
                                 </button>
                             </div>
                         </div>
                     </form>
                 </div>
+                <div class="panel-footer" style="padding: 2px; background-color: #FF9898;"></div>
             </div>
         </div>
     </div>
